957d96d72e460a413ed8a3083fbc5c908c27ae38
WasserX <wasserx@hotmail.com>
2012-01-10 17:01:09 +0100

    Refactored Node

--- src/Node.h
+++ src/Node.h
@@ -35,37 +35,4 @@ private:
 	std::string command;
 };
 
-Node::Node() {
-	nodeName = "";
-	isRule = false;
-	hasFinished = false;
-}
-
-Node::~Node() {
-}
-
-void Node::toString(){
-	using namespace std;
-
-	cout << "Node: " << nodeName << endl;
-	cout << "Is a Rule? " << isRule << endl;
-	cout << "Has Finished? " << hasFinished << endl;
-	cout << "Command: " << command << endl;
-
-	cout << "Dependencies: ";
-	for(list<Node*>::iterator it = dependencies.begin(); it != dependencies.end(); it++)
-		cout << (*it)->getNodeName() << " ";
-	cout << endl;
-
-	cout << "Solves: ";
-	for(list<Node*>::iterator it = solves.begin(); it != solves.end(); it++)
-		cout << (*it)->getNodeName() << " ";
-	cout << endl;
-
-	cout << "Terminals: ";
-	for(list<string>::iterator it = terminals.begin(); it != terminals.end(); it++)
-		cout << *it << " ";
-	cout << endl << endl;
-}
-
 #endif //NODE_H
